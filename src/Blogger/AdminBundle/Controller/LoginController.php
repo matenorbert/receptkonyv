7688e735452a831f32712c89fb0758a552fad325
Mate_Norbert <mate.norbert88@gmail.com>
2017-01-23 22:07:02 +0100

    beléptető rendszer

--- src/Blogger/AdminBundle/Controller/LoginController.php
+++ src/Blogger/AdminBundle/Controller/LoginController.php
@@ -12,45 +12,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
-use Blogger\AdminBundle\Entity as Entity;
 
 class LoginController extends Controller
 {
     /**
-     * @Route("/admin/login", name="login")
-     * @Method("POST")
+     * @Route("/admin/", name="login")
      */
-    public function validateAction(Request $request)
+    public function loginDefault(Request $request)
     {
-        $username = $request->request->get('username');
-        $pass = $request->request->get('userpass');
-        $token = $request->request->get('_validation_number');
+        return $this->render('BloggerAdminBundle:Default:index.html.twig', array('param' => 'beléptető'));
+    }
 
-        if($token !== $this->get('security.csrf.token_manager')->getToken('authenticate')->getValue())
-        {
-            return $this->render('BloggerAdminBundle:Default:index.html.twig', array('param' => 'Token hiba'));
-        }
+    /**
+     * @Route("/admin/loginsuccess", name="loginsuccess")
+     */
+    public function loginSuccess(Request $request)
+    {
+        return $this->render('BloggerAdminBundle:Default:dashboard.html.twig', array('username' => $this->getUser()));
+    }
 
-        $admin = new Entity\Admin();
-        $admin->setUserName($username);
-        $admin->setUserPass($pass);
+    /**
+     * @Route("/admin/loginfailure", name="loginfailure")
+     */
+    public function loginFailure(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
 
-        $validator = $this->container->get('validator');
-        $errors = $validator->validate($admin);
-
-        if(count($errors) > 0)
-        {
-            $msg = null;
-            foreach($errors as $key => $val)
-            {
-                $msg .= $errors[$key]->getMessage();
-            }
-
-            return $this->render('BloggerAdminBundle:Default:index.html.twig', array('param' => 'hiba van: ' . $msg));
-        }
-        else
-        {
-            return $this->render('BloggerAdminBundle:Default:index.html.twig', array('param' => 'nincshiba: ' . $username . $pass));
-        }
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        return $this->render('BloggerAdminBundle:Default:index.html.twig', array('param' => $error));
     }
 }
