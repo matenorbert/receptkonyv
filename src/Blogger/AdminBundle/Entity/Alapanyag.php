5e0b1abfdcca83162ef5340a6e2734ddd4da3a19
Mate_Norbert <mate.norbert88@gmail.com>
2017-01-31 22:11:02 +0100

    Adatbázis

---
+++ src/Blogger/AdminBundle/Entity/Alapanyag.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Blogger\AdminBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="Alapanyag")
+ */
+class Alapanyag
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Alapanyag
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
