73f9b97361a4c74342557f87ca99ec2a3629399d
Mate_Norbert <mate.norbert88@gmail.com>
2017-04-02 21:58:35 +0200

    new receipt

--- src/Blogger/AdminBundle/Resources/js/receipt.js
+++ src/Blogger/AdminBundle/Resources/js/receipt.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
-    $('body').on('click', '[name=addingredient]', function(){
-        $('.ingredientrow:last').clone().insertAfter('.ingredientrow:last');
+    $('body').on('click', '[name=receipt_addingredient]', function(){
+        $('.ingredientrow:first').clone().insertAfter('.ingredientrow:last').append('<button type="button" name="receipt_deleteingredient">-</button>');
+    });
+    $('body').on('click', '[name=receipt_deleteingredient]', function(){
+            $(this).parent().remove();
     });
 });
